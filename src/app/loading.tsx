f387c3fa48f3673a7af87e7e8f98c3aef4576754
Gemi <githuba122@gmail.com>
2024-12-28 16:13:55 +0700

    refactor env

---
+++ src/app/loading.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="grid grid-cols-4 place-items-center mt-5">
+        <div className="w-full max-w-sm bg-white rounded-lg shadow dark:bg-gray-400 my-5 h-96">
+          <a href="#"></a>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate"></h5>
+            </a>
+
+            <div className="flex items-center justify-between mt-3">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white"></span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full max-w-sm bg-white rounded-lg shadow dark:bg-gray-400 my-5 h-96">
+          <a href="#"></a>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate"></h5>
+            </a>
+
+            <div className="flex items-center justify-between mt-3">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white"></span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full max-w-sm bg-white rounded-lg shadow dark:bg-gray-400 my-5 h-96">
+          <a href="#"></a>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate"></h5>
+            </a>
+
+            <div className="flex items-center justify-between mt-3">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white"></span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full max-w-sm bg-white rounded-lg shadow dark:bg-gray-400 my-5 h-96">
+          <a href="#"></a>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate"></h5>
+            </a>
+
+            <div className="flex items-center justify-between mt-3">
+              <span className="text-3xl font-bold text-gray-900 dark:text-white"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -4,17 +4,17 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSL3uz2GceT-d8Q7ajlsS91mW2UNeS83o",
-  authDomain: "socialfy-ff817.firebaseapp.com",
-  projectId: "socialfy-ff817",
-  storageBucket: "socialfy-ff817.firebasestorage.app",
-  messagingSenderId: "889807161836",
-  appId: "1:889807161836:web:b17a8f930b2a8afaf298c9",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const storage = getStorage(app)
-const db = getFirestore(app)
+const storage = getStorage(app);
+const db = getFirestore(app);
 
 export { app, auth, storage, db };
